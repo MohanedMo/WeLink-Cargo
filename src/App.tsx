5aecb611794d8e308893fa6b8ca5a0530c409e1d
MohanedMo <mmhnd951@gmail.com>
2025-08-28 23:05:35 +0300

    Setup project

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const socket = new WebSocket("ws://localhost:3000/api/v1/ws");
+
+socket.onopen = () => {
+  console.log("Connected to server");
+  socket.send("Hello from client!");
+};
+
+socket.onmessage = (event) => {
+  console.log("Message from server:", event.data);
+};
+
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='text-red-800'>Welcome</h1>
+      <p>in my website</p>
     </>
   )
 }
